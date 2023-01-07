839fd27dc49940ebf83042a58a5e4bab6a3a8f31
jeanp <jeanpeaurroy@gmail.com>
2023-01-07 10:58:12 +0100

    lisibilité

--- JS/main.js
+++ JS/main.js
@@ -2,41 +2,19 @@ const inq = require('inquirer');
 
 const run = async () => {
 
-
-
-
-
     var a = inq.prompt(
         [
             {
             name: 'command',
             type: 'input',
             message: 'Entrez la commande: '
-
             }
         ]);
 
-
     a.then(function (answer) {
         console.log("entrée : " + answer.command);
-
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
 
 run();
 
