9dd4af5608a179c82a2b921dfd04a6c16aa78c4a
Lilian <delayel06@gmail.com>
2023-01-11 15:28:04 +0100

    read function finished, matrice result initialized

--- GO/main.go
+++ GO/main.go
@@ -8,30 +8,32 @@ import (
 	"strings"
 )
 
-func read(filename string) ([][]int, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
+func read(filename string) [][]int {
+	file, err := os.Open(filename) //on ouvre le fichier qui contient la matrice !!! attention renvoie fichier ET erreur
+	if err != nil {                //gestion de l'erreur pour voir si le fichier existe
+		fmt.Println("erreur ! j'arrive pas a ouvrir la matrice")
+		return nil
 	}
-	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	var matrice [][]int
 	for scanner.Scan() {
-		line := strings.Split(scanner.Text(), ",")
-
-		var row []int
-		for _, s := range line {
-
-			n, err := strconv.Atoi(s) // https://pkg.go.dev/strconv on convertit text en int parce que c'est plus cool
-
+		text := scanner.Text()
+		fmt.Print(text)              // error check, a enlever
+		temp := strings.Fields(text) //  Fields = split python mais en moins bien ici tout ca pour une boucle classique qui parcourt une ligne
+		fmt.Print(temp)              // error check, a enlever
+		var ligne []int
+		for i := 0; i < 3; i++ { //
+			//https://www.educative.io/answers/what-is-the-fields-function-in-go
+			n, err := strconv.Atoi(temp[i]) // convert string en entier https://pkg.go.dev/strconvl
 			if err != nil {
 				return nil
 			}
-			row = append(row, n)
+
+			ligne = append(ligne, n) //ajout nombre a ligne
 		}
 
-		matrice = append(matrice, row)
+		matrice = append(matrice, ligne) //ajout ligne a matrice
 	}
 
 	// Return the matrix
@@ -40,11 +42,21 @@ func read(filename string) ([][]int, error) {
 
 func main() { //Scanner fichier de la doc golang
 
-	matone := read("mat1.txt")
+	mat1 := read("mat1.txt")
+	mat2 := read("mat2.txt")         //peut pas utiliser mat2 encore
+	for i := 0; i < len(mat1); i++ { //j'arrive pas a afficher?
+		fmt.Println("%d ", mat1[i])
+		fmt.Print(",")
+	}
 
-	fmt.Print(matone)
-	mattwo := read("mat2.txt")
+	var result [][]int
+	for j := 0; j < len(mat1); j++ {
 
-	fmt.Print(mattwo)
+		var ligner []int
+		for k := 0; k < len(mat2[0]); k++ {
+			ligner = append(ligner, 0) // on rempli de 0 pour avoir une matrice résultat pseudo vide
+		}
+		result = append(result, ligner)
+	}
 
 }
