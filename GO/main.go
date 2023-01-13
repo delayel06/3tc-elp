e7cd187a5c47e9df78a4bb805651da834f9cbe9d
Lilian <delayel06@gmail.com>
2023-01-13 19:36:32 +0100

    phase 3 complete normalement tout marche il y avait un newline en plus sur un fichier matrice..

--- GO/main.go
+++ GO/main.go
@@ -14,23 +14,19 @@ import (
 func strtointsplice(s string) [][]int {
 	rows := strings.Split(s, "\n")
 	var splice [][]int
-
-	for i := 0; i < len(rows)-1; i++ { // ICI IMPORTANT : -1 car je regarde pas la derniere ligne, forcément parce que c nul fait pas parti matrice
+	for i := 0; i < len(rows); i++ {
 		cols := strings.Split(rows[i], " ")
 		var row []int // creer un truc a ajouter apres
 		for j := 0; j < len(cols); j++ {
+
 			num, _ := strconv.Atoi(cols[j])
+
 			row = append(row, num) // doit append parce que on a pas initializé de taille -> dynamique
 			//on ajout a chaque row le num puis chaque row jusqu'a la fin
 		}
 		splice = append(splice, row)
 	}
-	fmt.Println(splice)
-	fmt.Println(splice[0])
-	fmt.Println("lengths")
-	// tests
-	fmt.Println(len(splice))
-	fmt.Println(len(splice[0]))
+
 	return splice
 }
 func read(filename string) [][]int {
@@ -81,7 +77,8 @@ func process(mat1 [][]int, mat2 [][]int) [][]int {
 	for i := 0; i < len(mat1); i++ { // calc the matrice numbers yes
 		for j := 0; j < len(mat2[0]); j++ {
 			wg.Add(1)
-			go calc(mat1, mat2, result, i, j)
+			go calc(mat1, mat2, result, i, j) // concurrence fait tout seul insh
+			fmt.Print("fais calc\n")          // test
 			// tableaux modifiés globallement comme java
 		}
 	}
@@ -103,6 +100,8 @@ func tcp(c net.Conn) {
 	matreceived := string(stockage[:data])
 
 	result := process(strtointsplice(matreceived), read("mat2.txt"))
+	fmt.Println(strtointsplice(matreceived), "et autre : ", read("mat2.txt"))
+	fmt.Println("result:", result)
 	wg.Wait()
 
 	var outfile, errr = os.Create("result.txt")
