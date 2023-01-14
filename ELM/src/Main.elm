998965d93dd745305266c78cb44f878fddb119dc
JeanPrry <jeanpeaurroy@gmail.com>
2023-01-14 14:49:09 +0100

    ELM : Mise au propre de view possbilité de faire 'Enter' pour submit

--- ELM/src/Main.elm
+++ ELM/src/Main.elm
@@ -6,6 +6,7 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
 import Time
+import Keyboard exposing (RawKey)
 
 -- MAIN
 
@@ -23,11 +24,12 @@ type alias Model =
         , score : Int
         , timer : Int
         , httpState : State
+        , focus : Bool
     }
 
 init : () -> ( Model , Cmd Msg )
 init _ = 
-    ( Model "hello world" "" 0 60 Loading
+    ( Model "hello world" "" 0 180 Loading False
     , Http.get {
           url = "https://www.palabrasaleatorias.com/mots-aleatoires.php?fs=1&fs2=0&Submit=Nouveau+mot)"
         , expect = Http.expectString GotText
@@ -38,7 +40,7 @@ init _ =
 -- UPDATE
 
 type Msg
-    = Change String | Submit | Pass | GotText (Result Http.Error String) | Tick Time.Posix
+    = Change String | Submit | Pass | GotText (Result Http.Error String) | Tick Time.Posix | KeyDown RawKey | Focus | NotFocus
 
 update : Msg -> Model -> ( Model , Cmd Msg )
 update msg model = 
@@ -46,6 +48,8 @@ update msg model =
         Change word -> ( { model | wordSubmit = word } , Cmd.none )
 
         Submit -> if model.wordSubmit == model.wordToGuess then ( { model | score = model.score + 1 , wordSubmit = "" } , Cmd.none ) else ( { model | score = model.score - 1 , wordSubmit = "" } , Cmd.none )
+
+        KeyDown key -> if (Keyboard.anyKeyOriginal key) == Just Keyboard.Enter && model.focus == True then ( if model.wordSubmit == model.wordToGuess then ( { model | score = model.score + 1 , wordSubmit = "" } , Cmd.none ) else ( { model | score = model.score - 1 , wordSubmit = "" } , Cmd.none ) ) else (model , Cmd.none)
 
         Pass -> ( { model | wordSubmit = "" } , Cmd.none )
 
@@ -58,29 +62,50 @@ update msg model =
             Err _ ->
                 ({ model | httpState = Failure } , Cmd.none)
 
+        Focus -> ( { model | focus = True } , Cmd.none )
+
+        NotFocus -> ( { model | focus = False } , Cmd.none )
+
 
 -- SUBSCRIPTIONS
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    if model.timer > 0 then Time.every 1000 Tick else Sub.none
+    Sub.batch
+        [ if model.timer > 0 then Time.every 1000 Tick else Sub.none, Keyboard.downs KeyDown ]
 
 -- VIEW
 
 view : Model -> Html Msg
-view model = div [] [
-      h1 [style "color" "blue" ,  style "margin-left" "50px" , style "font-family" "verdana" , style "font-size" "300%", style "width" "100%", style "align" "center"] [ text "Elmphabetic"]
-    , div [ style "font-size" "150%" , style "margin" "8px 20px" ] [ div [style "float" "left", style "margin-left" "400px" , style "padding" "8px 20px"] [text ("Score : " ++ (String.fromInt model.score))] 
-             , div [style "float" "left",style "padding" "8px 20px"] [text ("TimeLeft : " ++ (String.fromInt model.timer))] ]
-    , div [style "width" "20%" , style "height" "50px"  , style "margin-left" "20px" , style "margin-top" "50px"] [ 
-        text """
-            Lorem ipsum dolor sit amet. Sit laborum quis ut voluptatem voluptas est nobis velit. Sit possimus nobis non harum natus et ipsa assumenda id voluptas 
-            provident in reiciendis autem est odit tempora. Qui reprehenderit obcaecati sed expedita officiis vel delectus voluptas sit voluptatem velit At expedita 
-            consequuntur qui ullam modi qui dolores dicta. Ut voluptas minus est iure quaerat qui excepturi nisi vel iste earum vel nisi officiis et fuga rerum sit
-            amet fuga. Eum aliquid dolorum aut optio veritatis ad galisum veniam a fugiat dolores aut enim saepe non dolorem eaque. Et vero necessitatibus sit quisquam
-            molestiae rem autem fuga et nihil quae rem fugiat internos a assumenda voluptatem. In cupiditate velit nam illum quam eum quas reprehenderit qui soluta
-            aperiam et sint tempora. Qui perspiciatis voluptate sit temporibus eaque id neque neque et recusandae quidem non nihil quaerat qui distinctio ipsa qui
-            dolor voluptatum! 33 sint corrupti sit suscipit praesentium ut quia cumque.
-            """
-        ], div[style "margin-left" "400px"] [div [] [input [ style "font-size" "20px",style "margin-left" "50px",style "margin-top" "180px", placeholder "Type your guess here !", value model.wordSubmit, onInput Change] [] ],div [] [button [ onClick Submit, style "padding" "15px 32px", style "margin-left" "50px" ] [ text "Submit"]
-                , button [onClick Pass,style "margin-top" "30px", style "padding" "15px 32px", style "margin-left" "20px" ] [ text "Pass" ] ] ]]
-
+view model =
+    div [] 
+        [
+          h1 [style "color" "blue" ,  style "margin-left" "50px" , style "font-family" "verdana" , style "font-size" "300%", style "width" "100%", style "align" "center"]
+             [ text "Elmphabetic"]
+        , div [ style "font-size" "150%" , style "margin" "8px 20px" ] 
+            [ div [style "float" "left", style "margin-left" "500px" , style "padding" "8px 20px"]
+                 [text ("Score : " ++ (String.fromInt model.score))] 
+            , div [style "float" "left",style "padding" "8px 20px"]
+                 [text ("TimeLeft : " ++ (String.fromInt model.timer))] ]
+        , div [style "width" "25%" , style "height" "50px"  , style "margin-left" "20px" , style "margin-top" "80px", style "font-size" "18px"] 
+            [ 
+            text """
+                Lorem ipsum dolor sit amet. Sit laborum quis ut voluptatem voluptas est nobis velit. Sit possimus nobis non harum natus et ipsa assumenda id voluptas 
+                provident in reiciendis autem est odit tempora. Qui reprehenderit obcaecati sed expedita officiis vel delectus voluptas sit voluptatem velit At expedita 
+                consequuntur qui ullam modi qui dolores dicta. Ut voluptas minus est iure quaerat qui excepturi nisi vel iste earum vel nisi officiis et fuga rerum sit
+                amet fuga. Eum aliquid dolorum aut optio veritatis ad galisum veniam a fugiat dolores aut enim saepe non dolorem eaque. Et vero necessitatibus sit quisquam
+                molestiae rem autem fuga et nihil quae rem fugiat internos a assumenda voluptatem. In cupiditate velit nam illum quam eum quas reprehenderit qui soluta
+                aperiam et sint tempora. Qui perspiciatis voluptate sit temporibus eaque id neque neque et recusandae quidem non nihil quaerat qui distinctio ipsa qui
+                dolor voluptatum! 33 sint corrupti sit suscipit praesentium ut quia cumque. 
+                """
+            ]
+        , div [ style "margin-left" "500px" ] 
+            [ div [] 
+                [ input [ style "font-size" "20px",style "margin-left" "50px",style "margin-top" "180px", placeholder "Type your guess here !", value model.wordSubmit, onInput Change, onFocus Focus ,onBlur NotFocus] [] ]
+            , div [] 
+                [ button [ onClick Submit, style "padding" "15px 32px", style "margin-left" "50px" ]
+                    [ text "Submit"]
+                , button [onClick Pass,style "margin-top" "30px", style "padding" "15px 32px", style "margin-left" "20px" ] 
+                    [ text "Pass" ]
+                ]
+            ]
+        ]
